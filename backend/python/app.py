4049c84986f4b1bc37996fba2e642733df682790
Troy Edwards <tedwards2@scu.edu>
2025-02-15 11:50:46 -0800

    app returns 501 on request to /api

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p> Hello world! </p>"
+
+@app.route("/api")
+def route_api():
+    dict = { "error": "not implemented" }
+    return jsonify(dict), 501
