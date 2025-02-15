36ff23fe4e6e7524f1465231a45f4cfc58e1d613
Troy Edwards <tedwards2@scu.edu>
2025-02-15 11:35:26 -0800

    fix app.py

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
-if __name__ == "__main__":
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p> Hello world! </p>"
+@app.route("/")
+def hello_world():
+    return "<p> Hello world! </p>"
